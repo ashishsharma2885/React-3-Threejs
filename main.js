a890998310f9d09b0dc98d2b812f9a9b678e7119
ashishsharma2885 <ashishsharma60141@gmail.com>
2026-01-04 20:30:59 +0530

    feat: initialize basic Three.js scene with rotating cube and lighting
    - Set up Three.js scene, camera, and renderer
    - Added a cube using BoxGeometry and MeshStandardMaterial
    - Configured directional light for proper illumination
    - Implemented animation loop for cube rotation
    - Rendered scene using WebGLRenderer

--- main.js
+++ main.js
@@ -4,3 +4,41 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
 scene.add(camera)
+
+camera.position.z = 5
+
+console.log(camera.position)
+
+/* MESH => (Shape and Material) */
+
+const cubeGemometry = new THREE.BoxGeometry(1,1,1)
+const cubeMaterial = new THREE.MeshStandardMaterial({
+  color:0x00FF00
+})
+
+const cube = new THREE.Mesh(cubeGemometry, cubeMaterial)
+scene.add(cube)
+
+console.log(cube.position)
+
+const light = new THREE.DirectionalLight(0xFFFFFF, 10)
+scene.add(light)
+
+light.position.y = 4
+light.position.z = 4
+
+console.log(light.position)
+
+const renedered = new THREE.WebGLRenderer()
+renedered.setSize(innerWidth, innerHeight)
+
+document.body.appendChild(renedered.domElement)
+
+function animate(){
+  cube.rotation.y += 0.05
+  cube.rotation.z += 0.05
+  cube.rotation.x += 0.05
+  renedered.render(scene, camera)
+}
+
+renedered.setAnimationLoop(animate)
